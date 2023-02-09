4c7d8b2414f9eca4dff01ab1eea542af06917744
Kristen Schaefer <kristencschaefer@gmail.com>
2023-02-09 16:58:23 -0600

    added category get route

--- models/index.js
+++ models/index.js
@@ -11,13 +11,24 @@ Product.belongsTo(Category, {
 
 // Categories have many Products
 Category.hasMany(Product, {
-  // **need to update with correct info
+  // **need to update with correct info - i think this is right
   foreignKey: 'category_id',
+
 });
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+  },
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+  },
+});
 
 module.exports = {
   Product,
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,10 +3,24 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
-});
+    try {
+      const categoryData = await Category.findAll();
+      const productData = await Product.findAll();
+      const returnData= [];
+      categoryData.forEach(category => {
+        let productsForCatogory = productData.filter(product=>product.category_id==category.id)
+        let returnProduct = productsForCatogory.map(product=>product.dataValues.product_name)
+        returnData.push({categoryName: category.category_name, products: returnProduct})
+      })
+      res.status(200).json(returnData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
